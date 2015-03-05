6f12a98ab178e377ab6e25cb6e195cdf520e4f1f
Samuel Rayment <samrayment@gmail.com>
2015-03-05 23:43:54 +0000

    Initial commit of the monitron control

---
+++ setup.py
@@ -0,0 +1,14 @@
+
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Monitron Command',
+      version='1.0',
+      description='Monitron Accessory Controller',
+      author='Sam Rayment',
+      author_email='',
+      url='',
+      packages=['monitron'],
+      scripts=['scripts/monitron'],
+     )
